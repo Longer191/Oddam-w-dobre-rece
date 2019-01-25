dd44ae5ee7a1cd117adafc0ff07259d309ae3f9b
Longer191 <j_michal7@o2.pl>
2019-01-25 16:01:56 +0100

    mobile form

--- js/mobile-form.js
+++ js/mobile-form.js
@@ -1,14 +1,71 @@
-  //mobile
+  //mobile choice menu
 
  $("#checkbox-1").click(function(){
-    $(".for-who").fadeToggle('slow');
+    $(".for-who").fadeToggle('fast');
   });
   $("#checkbox-4").click(function(){
-    $(".for-who-books").fadeToggle('slow');
+    $(".for-who-books").fadeToggle('fast');
   });
   $("#checkbox-5").click(function(){
-    $(".for-who-books-inne").fadeToggle('slow');
+    $(".for-who-books-inne").fadeToggle('fast');
   });
+
+  //slider
+
+  let sliderImages = document.querySelectorAll('.slide'),
+    sliderText = document.querySelectorAll('.change')
+    prevBtn = document.querySelector('.prevBtn'),
+    nextBtn = document.querySelector('.nextBtn'),
+    current = 0;
+
+    function reset() {
+      for (let i = 0; i < sliderImages.length; i++) {
+          sliderImages[i].style.display = "none";
+      }
+      for (let i = 0; i < sliderText.length; i++){
+        sliderText[i].style.display = 'none'
+      }
+  }
+  
+  function startSlide() {
+      reset();
+      sliderImages[0].style.display = "block";
+      sliderText[0].style.display = "block";
+    }
+  
+    function slideLeft() {
+      reset();
+      sliderImages[current - 1].style.display = "block";
+      sliderText[current - 1].style.display = "block";
+      current--;
+    }
+  
+    function slideRight() {
+      reset();
+      sliderImages[current + 1].style.display = "block";
+      sliderText[current + 1].style.display = "block";
+      current++;
+    }
+  
+    prevBtn.addEventListener("click", function() {
+      if (current === 0) {
+        current = sliderImages.length;
+        current = sliderText.length;
+      }
+      slideLeft();
+    });
+  
+    nextBtn.addEventListener("click", function() {
+      if (current === sliderImages.length - 1) {
+        current = -1;
+      }
+      if (current === sliderText.length - 1) {
+        current = -1;
+      }
+      slideRight();
+    });
+    startSlide();
+  
 
 
 
