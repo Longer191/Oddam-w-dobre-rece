93abbdf4e7a6d16ac0db16dcb1dc4b442d41f0a1
Longer191 <j_michal7@o2.pl>
2019-02-04 19:48:08 +0100

    correction

--- js/mobile-form.js
+++ js/mobile-form.js
@@ -17,6 +17,7 @@
     prevBtn = document.querySelector('.prevBtn'),
     nextBtn = document.querySelector('.nextBtn'),
     current = 0;
+    console.log(sliderImages)
 
     function reset() {
       for (let i = 0; i < sliderImages.length; i++) {
@@ -65,6 +66,99 @@
       slideRight();
     });
     startSlide();
+
+    //getting value of inputs
+let counter = 0;
+
+let nameOrg = document.getElementById('name-of-organisation');
+let numBags = document.getElementById('num-of-given-bags');
+
+function getVal() {
+  //street
+  let streetOut = document.getElementById('streetOut');
+  let street = document.getElementById('street');
+  //city
+  let city = document.getElementById('city');
+  let cityOut = document.getElementById('cityOut');
+  //postal
+  let postal = document.getElementById('postal');
+  let postalOut = document.getElementById('postalOut');
+  //telephone
+  let telephone = document.getElementById('telephone');
+  let telephoneOut = document.getElementById('telephoneOut');
+  //date
+  let date = document.getElementById('date');
+  let dateOut = document.getElementById('dateOut');
+  //time
+  let time = document.getElementById('time');
+  let timeOut = document.getElementById('timeOut');
+  //comment
+  let comment = document.getElementById('comment');
+  let commentOut = document.getElementById('commentOut');
+
+  nextBtn.addEventListener('click', () => {
+    counter ++;
+    streetOut.value = street.value;
+    cityOut.value = city.value;
+    postalOut.value = postal.value;
+    telephoneOut.value = telephone.value;
+    dateOut.value = date.value;
+    timeOut.value = time.value;
+    commentOut.value = comment.value;
+    console.log(nextBtn)
+    if (counter == 5) {
+      document.querySelector('.nextBtn').innerText = "Potwierdzam";
+      document.querySelector('.nextBtn').style.width = "150px"
+    } else if (counter == 6) {
+      document.querySelector('.nextBtn').style.display = "none";
+      document.querySelector('.prevBtn').style.display = "none";
+    }
+  })
+}
+
+getVal();
+
+//getting checkbox values from the slides
+
+const multiCheck = () => {
+  //values of selected checkboxes from the first slide
+  let result = '';
+  let checkLength = document.querySelectorAll('input[name="coder"]');
+  for (let i = 0; i < checkLength.length; i++) {
+    let checkedVal = checkLength[i].checked;
+    if (checkedVal) {
+      result += checkLength[i].value;
+    }
+  }
+  let showData = result;
+  //values of selected options from the second slide
+  let bagNumClass = document.querySelector('.numbers-bags');
+  let selectedNumberOfBags = bagNumClass.options[bagNumClass.selectedIndex].value;
+  //values of selected options from the third slide
+  let cityChoice = document.querySelector('.citiesNumber');
+  let selectedCity = cityChoice.options[cityChoice.selectedIndex].value;
+  //for whom the items will be given
+  let targetChoice = document.querySelectorAll('.options-btn');
+  for (let i = 0; i < targetChoice.length; i++) {
+    targetChoice[i].addEventListener('click', (e) => {
+      let targetChoiceValue = targetChoice[i].value;
+      numBags.innerHTML = selectedNumberOfBags + ' worki ' + showData + ' dla ' + targetChoiceValue;
+    })
+  }
+  //name of the organisation
+  let resultOrg = '';
+  let orgLenght = document.querySelectorAll('input[name="org-name"]');
+  for (let i = 0; i < orgLenght.length; i++) {
+    let checkedOrg = orgLenght[i].checked;
+    if (checkedOrg) {
+      resultOrg += orgLenght[i].value;
+    }
+  }
+  let showOrg = resultOrg;
+  nameOrg.innerHTML = showOrg + selectedCity;
+}
+
+multiCheck()
   
 
 
